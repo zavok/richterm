6410f18e2de62d15ad3334bdac10ae8124e42d0d
Pavel Renev <an2qzavok@gmail.com>
2021-07-31 21:27:18 +0000

    refactor mdprint

--- extra/mdprint.c
+++ extra/mdprint.c
@@ -1,6 +1,83 @@
 #include <u.h>
 #include <libc.h>
 #include <bio.h>
+#include <String.h>
+
+char * newobj(void);
+void parse(char);
+void purge(void);
+
+int newblock = 1;
+String *bbuf;
+
+void
+main(int argc, char **argv)
+{
+	int fd;
+	Biobuf *bp;
+	char c;
+
+	if (argc > 1) {
+		if ((fd = open(argv[1], OREAD)) < 0)
+			sysfatal("can't open %s, %r", argv[1]);
+	} else fd = 0;
+	bp = Bfdopen(fd, OREAD);
+	bbuf = s_new();
+
+	while ((c = Bgetc(bp)) >= 0) {
+		parse(c);
+	}
+}
+
+void
+parse(char c)
+{
+	int purgeblock;
+
+	purgeblock = 0;
+
+	if (newblock != 0) {
+		newblock = 0;
+		/* ... */
+	} else {
+		/* ... */
+	}
+	
+	bbuf = s_nappend(bbuf, &c, 1);
+
+	if (c == '\n') purgeblock = 1;
+
+	if (purgeblock != 0) {
+		newblock = 1;
+		/* ... */
+		purge();
+		bbuf = s_reset(bbuf);
+	}
+}
+
+void
+purge(void)
+{
+	char *id, *path;
+	int td;
+	usize n, size;
+
+	id = newobj();
+	path = smprint("/mnt/richterm/%s/text", id);
+
+	td = open(path, OWRITE);
+	if (td < 0) sysfatal("%r");
+
+	size = bbuf->end - bbuf->base;
+
+	n = write(td, bbuf->base, size);
+
+	if (n != size)
+		sysfatal("write failed: %r");
+	close(td);
+	free(path);
+	
+}
 
 char *
 newobj(void)
@@ -13,35 +90,4 @@ newobj(void)
 	read(fd, buf, 256);
 	close(fd);
 	return buf;
-}
-
-void
-main(int argc, char **argv)
-{
-	int fd;
-	long n;
-	Biobuf *bp;
-	char *lp;
-	if (argc > 1) {
-		if ((fd = open(argv[1], OREAD)) < 0)
-			sysfatal("can't open %s, %r", argv[1]);
-	} else fd = 0;
-	bp = Bfdopen(fd, OREAD);
-	while ((lp = Brdstr(bp, '\n', 0)) != nil) {
-		char *id;
-		char *path;
-		int td;
-		id = newobj();
-		path = smprint("/mnt/richterm/%s/text", id);
-		print("%s ", id);
-		td = open(path, OWRITE);
-		if (td < 0) sysfatal("%r");
-		n = write(td, lp, strlen(lp));
-		if (n != strlen(lp))
-			sysfatal("write failed, %ld/%ld", n, strlen(lp));
-		close(td);
-		free(lp);
-		free(id);
-		free(path);
-	}
 }
