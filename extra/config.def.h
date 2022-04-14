65fa08ce1b68303d0339cd91511c7261c5ae69dc
Pavel Renev <an2qzavok@gmail.com>
2022-04-14 21:35:46 +0000

    add extra/config.def.h

---
+++ extra/config.def.h
@@ -0,0 +1,14 @@
+enum {
+	Fheader1, Fheader2, Fheader3, Fheader4, Fheader5, Fheader6,
+	Fcode,
+};
+
+char *fonts[] = {
+	[Fheader1] "/lib/font/bit/lucida/unicode.20.font",
+	[Fheader2] "/lib/font/bit/lucida/unicode.18.font",
+	[Fheader3] "/lib/font/bit/lucida/unicode.16.font",
+	[Fheader4] "/lib/font/bit/lucida/unicode.14.font",
+	[Fheader5] "/lib/font/bit/lucida/unicode.12.font",
+	[Fheader6] "/lib/font/bit/lucida/unicode.10.font",
+	[Fcode] "/lib/font/bit/terminus/unicode.16.font",
+};
