60da4cbe2337722dc0e86ce5cb4ab598e2243d60
Pavel Renev <an2qzavok@gmail.com>
2022-04-29 01:56:53 +0000

    Dir: some tinkering

--- extra/Dir.c
+++ extra/Dir.c
@@ -27,15 +27,14 @@ main(int argc, char **argv)
 		dbuf = mallocz(DIRMAX, 1);
 		n = dirreadall(fd, &dbuf);
 
-		print("f\nl..\n.../\nn\n");
+		print("f\n" "l..\n" ".../\n" "l\n" "n\n");
 
 		for (i = 0; i < n; i++) {
 			char *spacer = "";
 			if (dbuf[i].mode & DMEXEC) spacer = "*";
 			if (dbuf[i].mode & DMDIR) spacer = "/";
 
-			print("l%s\n.%s%s\nn\n", dbuf[i].name, dbuf[i].name, spacer);
+			print("l%s\n" ".%s%s\n" "l\n" "n\n", dbuf[i].name, dbuf[i].name, spacer);
 		}
-		print("l\n");
 	} else sysfatal("not a directory");
 }
